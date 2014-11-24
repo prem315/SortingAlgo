2edeca12b382243db0ad17e6e537171500535c0b
Prem Patel <prem.patel3105@gmail.com>
2014-11-24 10:27:25 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,37 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Prem
- */
 public class SelectionSort {
-    
+    public int[] a;
     public static int[] sort(int[] a){
         
         int n = a.length-1;
         for(int i=0;i<=n;i++){
             for(int j=i+1;j<=n;j++){
                 if(a[i]>a[j]){
-                    //swap
-                    int temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
+                    swap(a,i,j);
+               //     int temp = a[i];
+                 //   a[i] = a[j];
+                   // a[j] = temp;
                 }
             }
         }
         return a;
         
     }
-/*    
-    public void swap(int[] a,int p, int q){
+    
+    public static int swap(int[] a,int p, int q){
         int temp = a[p];
         a[p] = a[q];
         a[q] = temp;
+        return 0;
     }
-  */  
+    
     public static void main(String ar[]){
         int[] a = {5,6,2,4,9,10,23,15,7,0};
         int[] b = sort(a);
